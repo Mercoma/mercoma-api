172abfeabcd8f9d6b14d34e02d8000d5fcca3b53
julian-hecker <julianheckerdev@gmail.com>
2020-06-06 03:55:28 -0400

    create image routes

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -11,14 +11,58 @@ module.exports.getAll = (req, res) => {
         });
 };
 
-module.exports.getFromUser = (req, res) => {
-    Image.findAll({
+module.exports.getById = (req, res) => {
+    const { imageId } = req.params;
+    Image.findOne({
         where: {
-            userId: req.params.userId,
+            id: imageId,
         },
     })
         .then((images) => {
             res.status(200).json(images);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send(err);
+        });
+};
+
+module.exports.add = (req, res) => {
+    const { userId } = req.user;
+    Image.create({})
+        .then((image) => {})
+        .catch((err) => {
+            console.log(err);
+            res.send(err);
+        });
+};
+
+module.exports.getFromUser = (req, res) => {
+    const { userId } = req.user;
+    Image.findAll({
+        where: {
+            userId: userId,
+        },
+    })
+        .then((images) => {
+            res.status(200).json(images);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send(err);
+        });
+};
+
+module.exports.remove = (req, res) => {
+    const { imageId } = req.params;
+    Image.destroy({
+        where: {
+            id: imageId,
+        },
+        limit: 1,
+    })
+        .then((image) => {
+            res.status(200);
         })
         .catch((err) => {
             console.log(err);
--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
 const images = require('../controllers/imageController');
 
+// Get all of a user's images
 router.get('/', (req, res) => {
-    images.getAll(req, res);
+    images.getFromUser(req, res);
 });
 
-router.get('/:userId', (req, res) => {
-    images.getFromUser(req, res);
+// Create a new image
+router.post('/', (req, res) => {
+    images.add(req, res);
+})
+
+// Get a single image by ID
+router.get('/:imageId', (req, res) => {
+    images.getById(req, res);
+});
+
+// Delete a single image by ID
+router.delete('/:imageId', (req, res) => {
+    images.remove(req, res);
 });
 
 module.exports = router;
