aa5475b2aca2b5b500b625cbb55bf0c28e969b27
julian-hecker <julianheckerdev@gmail.com>
2020-06-07 00:37:42 -0400

    try fix cors D:

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(
         secure: false,
     }),
 );
-app.use(cors({ origin: true }));
+app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(buildDirectory));
